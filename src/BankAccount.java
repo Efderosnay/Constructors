da434e5860d95139c11cc3368b94688950287791
Eugenie Fromet de Rosnay <e.fderosnay@gmail.com>
2019-06-05 12:25:53 +0100

    Initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,83 @@
+public class BankAccount {
+
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+    public BankAccount(){
+        this("56789", 2.50, "Default name", "Default address", "default phone");
+        //this is a default constructor  --  This is calling another constructor within a constructor.
+        System.out.println("Empty constructor called");
+    }
+
+    public BankAccount(String number, double balance, String customerName, String customerEmailAddress, String customerPhoneNumber){
+        System.out.println("Account constructor with parameters called");
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmailAddress = customerEmailAddress;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public BankAccount(String customerName, String customerEmailAddress, String customerPhoneNumber){
+        this("99999", 100.55, customerName, customerEmailAddress, customerPhoneNumber);
+    }
+
+    public void deposit (double depositAmount){
+        this.balance += depositAmount;
+        // += add to the balance
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
+    }
+
+    public void withdrawal (double withdrawalAmount){
+        if(this.balance - withdrawalAmount < 0){
+            System.out.println("Only " + this.balance + " available. Withdrawal not processed");
+        } else {
+            this.balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance = " + this.balance);
+        }
+    }
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a new class for a bank account
+        // Create fields for the account number, balance, customer name, email and phone number.
+        //
+        // Create getters and setters for each field
+        // Create two additional methods
+        // 1. To allow the customer to deposit funds (this should increment the balance field).
+        // 2. To allow the customer to withdraw funds. This should deduct from the balance field,
+        // but not allow the withdrawal to complete if their are insufficient funds.
+        // You will want to create various code in the Main class ( the one created by IntelliJ) to
+        // confirm your code is working.
+        // Add some System.out.println's in the two methods above as well.
+//
+//        BankAccount bobAccount = new BankAccount("12345", 0.00, "Bob Brown","myemail@bob.com","(087) 123-4567");
+//
+//        System.out.println(bobAccount.getNumber());
+//        System.out.println(bobAccount.getBalance());
+//        bobAccount.withdrawal(100.0);
+//
+//        bobAccount.deposit(50.0);
+//        bobAccount.withdrawal(100.0);
+//        bobAccount.deposit(51.0);
+//        bobAccount.withdrawal(100.0);
+//
+//        BankAccount timsAccount = new BankAccount("Tim", "tim@email.com", "12345");
+
+        // Create a new class VipCustomer
+        // it should have 3 fields name, credit limit, and email address.
+        // create 3 constructors
+        // 1st constructor empty should call the constructor with 3 parameters with default values
+        // 2nd constructor should pass on the 2 values it received and add a default value for the 3rd
+        // 3rd constructor should save all fields.
+        // create getter only for this using code generation of intelliJ as setters wont be needed
+        // test and confirm it works
+
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        System.out.println(person2.getName());
+
+        VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());
+    }
+}
+
